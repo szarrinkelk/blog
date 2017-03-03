21efafb02a7c23b8ac0e49a02ac5dcaca69594a4
szarrinkelk <szarrinkelk@gmail.com>
2017-03-03 15:52:46 +0100

    migrations

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -11,7 +11,7 @@ class Blog(models.Model):
     slug = models.SlugField(max_length=100, unique=True)
     body = models.TextField()
     posted = models.DateTimeField(db_index=True, auto_now_add=True)
-    category = models.ForeignKey('blog.Category')
+    category = models.ForeignKey('myblog.Category')
 
     def __unicode__(self):
         return '%s' % self.title
